fe6a3f69b24773e8f708e85045d19aa0443094db
Mark Hay <mark@haym.me>
2023-01-21 13:53:16 -0800

    run node and capture stderr

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
+use std::process::{Command, Stdio};
+use std::{
+    fs::File,
+    io::{self, BufReader, BufRead},
+};
+
+fn main() -> io::Result<()> {
     println!("Hello, world!");
+
+    let mut node_process = Command::new("node")
+        .arg("test.js")
+        .stderr(Stdio::piped())
+        .spawn()
+        .expect("Failed to start node");
+    let reader = BufReader::new(node_process.stderr.take().expect("failed to capture stdout"));
+    for line in reader.lines() {
+        println!("ERROR: {}",line?);
+    }
+    Ok(())
+
 }
